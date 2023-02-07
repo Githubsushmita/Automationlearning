acab371c3805b4bc0a27871130de7a0c842d3b4f
SUSHMITA1 <SUSHMITA1@BG6X4M3.discovery.holdings.co.za>
2023-02-07 14:44:03 +0200

    commit 3rd time

--- Project_02/src/Pkg1/GithubEX2.java
+++ Project_02/src/Pkg1/GithubEX2.java
@@ -1,5 +1,16 @@
 package Pkg1;
 
-public class GithubEX2 {
-
+public class GithubEX2
+{
+	public static void main(String[] args)
+	
+	{
+		System.out.println("Second class added for Github example");
+		
+		
+	}
+	
+	
+	
+	
 }
